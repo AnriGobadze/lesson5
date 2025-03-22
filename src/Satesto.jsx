6f9eac34f6a0f2313aded8ad24f9cfbd3d089606
anri gobadze <anrigobadze02@gmail.com>
2025-03-22 16:44:48 +0400

    upgrade

--- src/Satesto.jsx
+++ src/Satesto.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 export default function Satesto({ product }) {
   return (
     <>
-      <div className="card">
+      <div className="card" style={product.price > 100 ? {border: '2px solid black'} : {border: '2px solid red'}}>
         <img src={product.image} alt={product.title} />
         <h2>{product.title}</h2>
+        <h2>{product.price}$</h2>
         <h5>{product.category}</h5>
         <p>{product.description.slice(0, 200)}...</p>
       </div>
